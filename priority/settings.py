ef75cae69cc3c0c7a59d54bbcacda3c067afe681
Nick Carver <nicholascarver2@outlook.com>
2021-06-10 10:15:13 -0500

    added a comment about the secret key

--- priority/settings.py
+++ priority/settings.py
@@ -21,6 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# if running locally, use source .env to add the secret key to the environment variables.
+# the .env file should look like export SECRET_KEY="the key"
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
