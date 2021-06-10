68fb9b1966bd6eabe7daf69bffb64ffbf3dafaea
Nick Carver <nicholascarver2@outlook.com>
2021-06-10 11:13:54 -0500

    created all the models

---
+++ priorityapi/models/affirmation.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+
+class Affirmation(models.Model):
+
+    priority_user = models.ForeignKey("PriorityUser", on_delete=models.CASCADE)
+    priority = models.ForeignKey("Priority", on_delete=models.CASCADE)
+    affirmation = models.CharField(max_length=200)
+    created_on = models.DateTimeField(default=timezone.now())
---
+++ priorityapi/models/history.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+
+class History(models.Model):
+
+    what = models.ForeignKey("What", on_delete=models.CASCADE)
+    submission_date = models.DateTimeField(default=timezone.now())
+    goal_date = models.DateField()
+    time_spent = models.IntegerField()
