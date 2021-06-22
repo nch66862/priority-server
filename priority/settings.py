d4ecd3bd122357ca3f02828b6d97d7c7bb95133b
Nick Carver <nicholascarver2@outlook.com>
2021-06-22 10:58:44 -0500

    added the right one

--- priority/settings.py
+++ priority/settings.py
@@ -73,7 +73,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://nac-priority.herokuapp.com'
+    'https://nac-priority.netlify.app'
 )
 
 ROOT_URLCONF = 'priority.urls'
