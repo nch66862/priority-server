51d85fa2263009f3d00d64e2c90113b2a1cea3bd
Nick Carver <nicholascarver2@outlook.com>
2021-06-22 10:58:04 -0500

    added the deployed site url to the white list. but the wrong one

--- priority/settings.py
+++ priority/settings.py
@@ -72,7 +72,8 @@ REST_FRAMEWORK = {
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000'
+    'http://127.0.0.1:3000',
+    'https://nac-priority.herokuapp.com'
 )
 
 ROOT_URLCONF = 'priority.urls'
